be9672768a9c50e271b49e05dbe6eb80362c9beb
Participant <vishal580.das@gmail.com>
2026-07-21 23:02:22 +0530

    Add files via upload
    pwm.c and pwm.h files initialised

---
+++ Drivers/pwm.c
@@ -0,0 +1,14 @@
+#include "pwm.h"
+#include"msp430h"
+#include"defines.h"
+
+
+
+// static bool initialised = false;
+void pwm_init(void)
+{
+   // ASSERT(!initialised);
+
+}
+
+
---
+++ Drivers/pwm.h
@@ -0,0 +1,17 @@
+#ifndef PWM_H
+#define PWM_H
+
+#include"stdint.h"
+
+typedef  enum
+{
+    PWM_MOTORDRIVER_LEFT,  
+    PWM_MOTORDRIVER_RIGHT
+}pwm_e
+
+
+void pwm_init(void);
+void pwm_duty_cycle(pwm_e pwm,uint8_t duty_cycle_percent);
+
+
+#endif
